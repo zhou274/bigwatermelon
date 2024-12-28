0d6b947c517ff19a698124f4f2eede7b8c386d71
zhou274 <274891948@qq.com>
2024-12-28 14:14:30 +0800

    1.0

--- big-watermelon-master/Assets/Scripts/GameClockUI.cs
+++ big-watermelon-master/Assets/Scripts/GameClockUI.cs
@@ -4,6 +4,7 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
+
 public class GameClockUI : MonoBehaviour
 {
     [SerializeField] private GameObject uiParent;
--- big-watermelon-master/Assets/Scripts/GameOverUI.cs
+++ big-watermelon-master/Assets/Scripts/GameOverUI.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using StarkSDKSpace;
+using TTSDK.UNBridgeLib.LitJson;
+using TTSDK;
 
 
 public class GameOverUI : MonoBehaviour
 {
     public TextMeshProUGUI BestScore;
+    private StarkAdManager starkAdManager;
+    public string clickid;
     // Start is called before the first frame update
     public void OnEnable()
     {
@@ -20,8 +25,88 @@ public class GameOverUI : MonoBehaviour
     }
     public void Continue()
     {
-        PlayerManager.Instance.isGameOver = false;
-        PlayerManager.Instance.time = PlayerManager.Instance.PlayTimeTotal;
-        gameObject.SetActive(false);
+        ShowVideoAd("192if3b93qo6991ed0",
+            (bol) => {
+                if (bol)
+                {
+                    
+                    Debug.Log("xxx");
+                    PlayerManager.Instance.isGameOver = false;
+                    PlayerManager.Instance.time = PlayerManager.Instance.PlayTimeTotal;
+                    gameObject.SetActive(false);
+                    clickid = "";
+                    getClickid();
+                    apiSend("game_addiction", clickid);
+                    apiSend("lt_roi", clickid);
+                }
+                else
+                {
+                    StarkSDKSpace.AndroidUIManager.ShowToast("观看完整视频才能获取奖励哦！");
+                }
+            },
+            (it, str) => {
+                Debug.LogError("Error->" + str);
+                //AndroidUIManager.ShowToast("广告加载异常，请重新看广告！");
+            });
+        //PlayerManager.Instance.isGameOver = false;
+        //PlayerManager.Instance.time = PlayerManager.Instance.PlayTimeTotal;
+        //gameObject.SetActive(false);
+    }
+    public void getClickid()
+    {
+        var launchOpt = StarkSDK.API.GetLaunchOptionsSync();
+        if (launchOpt.Query != null)
+        {
+            foreach (KeyValuePair<string, string> kv in launchOpt.Query)
+                if (kv.Value != null)
+                {
+                    Debug.Log(kv.Key + "<-参数-> " + kv.Value);
+                    if (kv.Key.ToString() == "clickid")
+                    {
+                        clickid = kv.Value.ToString();
+                    }
+                }
+                else
+                {
+                    Debug.Log(kv.Key + "<-参数-> " + "null ");
+                }
+        }
+    }
+
+    public void apiSend(string eventname, string clickid)
+    {
+        TTRequest.InnerOptions options = new TTRequest.InnerOptions();
+        options.Header["content-type"] = "application/json";
+        options.Method = "POST";
+
+        JsonData data1 = new JsonData();
+
+        data1["event_type"] = eventname;
+        data1["context"] = new JsonData();
+        data1["context"]["ad"] = new JsonData();
+        data1["context"]["ad"]["callback"] = clickid;
+
+        Debug.Log("<-data1-> " + data1.ToJson());
+
+        options.Data = data1.ToJson();
+
+        TT.Request("https://analytics.oceanengine.com/api/v2/conversion", options,
+           response => { Debug.Log(response); },
+           response => { Debug.Log(response); });
+    }
+
+
+    /// <summary>
+    /// </summary>
+    /// <param name="adId"></param>
+    /// <param name="closeCallBack"></param>
+    /// <param name="errorCallBack"></param>
+    public void ShowVideoAd(string adId, System.Action<bool> closeCallBack, System.Action<int, string> errorCallBack)
+    {
+        starkAdManager = StarkSDK.API.GetStarkAdManager();
+        if (starkAdManager != null)
+        {
+            starkAdManager.ShowVideoAdWithId(adId, closeCallBack, errorCallBack);
+        }
     }
 }
